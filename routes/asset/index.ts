f6a602f7128dcf701f8183e4732aea7c22f5c534
SecretCodingStar <t56200840@gmail.com>
2024-12-16 06:22:31 -0500

    12/16

--- routes/asset/index.ts
+++ routes/asset/index.ts
@@ -4,5 +4,6 @@ import * as assetController from "../../controllers/asset";
 const router = express.Router();
 
 router.get("/init", assetController.init);
+router.post("/insert", assetController.insert);
 
 module.exports = router;
