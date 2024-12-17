24d7e3f78d284416bfbd2223a423fa9c0e5a8199
undyingkevin <williamadams19901111@gmail.com>
2024-12-17 14:04:35 -0500

    paypal integration

--- routes/payment/index.ts
+++ routes/payment/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import { authorizeBearerToken } from "../../middlewares/auth";
 import * as stripeController from "../../controllers/payment/stripe";
 import * as paypalController from "../../controllers/payment/paypal";
 const router = express.Router();
 
-router.post("/stripe/session-complete", stripeController.sessionComplete);
-router.post("/stripe/session-initiate", stripeController.sessionInitiate);
-router.post("paypal", )
+router.post("/stripe/session-complete", authorizeBearerToken, stripeController.sessionComplete);
+router.post("/stripe/session-initiate", authorizeBearerToken, stripeController.sessionInitiate);
+router.post("paypal/pay", authorizeBearerToken, paypalController.pay);
+router.post("paypal/success", authorizeBearerToken, paypalController.success);
+router.post("paypal/cancel", authorizeBearerToken, paypalController.cancel);
+
 
 module.exports = router;
