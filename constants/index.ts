74e31e62b629fa89085336f1b9daf047f05194e3
unknown <esaibrilliant34310@gmail.com>
2024-12-24 20:08:44 -0800

    merge dev

--- constants/index.ts
+++ constants/index.ts
@@ -6,14 +6,14 @@ export const baseClientUrl = "https://howlucky2025.com"; // in live env
 // export const baseClientUrl = "http://localhost:5173"; // in dev env
 
 // Stripe env
-export const webHookKey = process.env.STRIPE_WEBHOOK_SECRET_TEST ?? (() => { throw new Error("Missing Stripe Webhook Key"); })();
-export const secretKey = process.env.STRIPE_SECRET_KEY_TEST ?? (() => { throw new Error("Missing Stripe Secret Key"); })();
-export const priceIds = process.env.STRIPE_PRICE_IDS_TEST ?? (() => { throw new Error("Missing Stripe Price ID"); })();
+export const webHookKey = process.env.STRIPE_WEBHOOK_SECRET ?? (() => { throw new Error("Missing Stripe Webhook Key"); })();
+export const secretKey = process.env.STRIPE_SECRET_KEY ?? (() => { throw new Error("Missing Stripe Secret Key"); })();
+export const priceIds = process.env.STRIPE_PRICE_IDS ?? (() => { throw new Error("Missing Stripe Price ID"); })();
 //Paypal env
-export const clientId = process.env.PAYPAL_CLIENT_ID_TEST ?? (() => { throw new Error("Missing Paypal Client ID"); })();
-export const clientSecret = process.env.PAYPAL_CLIENT_SECRET_TEST ?? (() => { throw new Error("Missing Paypal Client Secret"); })();
+export const clientId = process.env.PAYPAL_CLIENT_ID ?? (() => { throw new Error("Missing Paypal Client ID"); })();
+export const clientSecret = process.env.PAYPAL_CLIENT_SECRET ?? (() => { throw new Error("Missing Paypal Client Secret"); })();
 // export const PAYPAL_MODE = "live";
-export const PAYPAL_MODE = "sandbox";
+export const PAYPAL_MODE = "live";
 
 export const GLOBAL_ERRORS = {
   serverError: "An unexpected error occurred",
